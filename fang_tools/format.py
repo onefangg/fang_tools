2bd12a13899508915280b4e00a978acfe07743d3
onefangg <onefangg@gmail.com>
2023-03-12 21:03:40 +0800

    adding lother libraries to curr lib

--- fang_tools/format.py
+++ fang_tools/format.py
@@ -1,7 +1,16 @@
 """
 Module for formatting text
 """
+from rich.console import Console
+from rich.text import Text
 
 
 def starry_text(text):
     return f"****{text}****"
+
+
+def pprint_starry_text(text, color="bold magenta"):
+    console = Console()
+    fmt_text = Text(text)
+    fmt_text.stylize(color, 0, 6)
+    console.print(fmt_text)
--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="fang_tools",
-    version="0.0.1",
+    version="0.0.2",
     python_requires=">=3.7",
     packages=find_packages(),
+    install_requires=[
+        "rich>=13.0.0"
+    ]
 )
